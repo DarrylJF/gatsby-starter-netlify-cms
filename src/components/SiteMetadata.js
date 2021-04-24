46502bcd3aede66f40d2c89788a6dfbec12986ac
DJF <darryljf.develop@gmail.com>
2021-04-24 12:36:34 +0100

    changed siteMetadata 'title'

--- src/components/SiteMetadata.js
+++ src/components/SiteMetadata.js
@@ -1,19 +1,18 @@
-import { graphql, useStaticQuery } from 'gatsby'
+import {graphql, useStaticQuery} from 'gatsby'
 
 const useSiteMetadata = () => {
-  const { site } = useStaticQuery(
-    graphql`
+	const {site} = useStaticQuery(
+		graphql`
       query SITE_METADATA_QUERY {
         site {
           siteMetadata {
             title
-            description
           }
         }
       }
     `
-  )
-  return site.siteMetadata
+	)
+	return site.siteMetadata
 }
 
 export default useSiteMetadata
